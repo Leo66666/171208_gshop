fe326f50a3cf8ee02a0989bec9b6083acf1c025e
廖晨希 <lsz45007@qq.com>
2019-01-08 19:16:56 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="App">
+    <router-view/>
+
+    <!-- 只有在当前路由不等于login的时候才显示footer -->
+    <FooterGuide v-show="$route.meta.showFooter" />
+  </div>
+</template>
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+export default {
+  components: {
+    FooterGuide
+  }
+}
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+  .app
+    width:100%
+    height:100%
+    background #f5f5f5
+</style>
